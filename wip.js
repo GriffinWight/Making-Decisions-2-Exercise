cd53cf3d10c4365f2fba6631b34f286e55705e80
Griffin Wight <Griffin@wight.net>
2022-02-26 12:31:23 -0600

    Initial commit

---
+++ wip.js
@@ -0,0 +1,20 @@
+////////// Advanced Problems //////////
+
+////////// PROBLEM 10 //////////
+// Switch statements can be excellent alternatives to if blocks. Look up switch statements (I recommend W3 Schools) and try to implement one for the following.
+
+// Do not edit the code below.
+let letterGrade = 'B'
+// Do not edit the code above.
+
+/* Use a switch statement on 'letterGrade' and console.log the appropriate response.
+
+If A: "The student is doing excellently."
+If B: "The student is doing well."
+If C: "The student is doing alright."
+If D: "The student is not doing very well."
+If F: "The student is failing."
+If the letter grade is not one of the above letters, console.log 'Not an eligible grade.'
+*/
+
+//Code Here
